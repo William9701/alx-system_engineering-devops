a398e5a155038d05f4400e617e190fd39c196fac
Obi <williamobi818@gmail.com>
2023-09-23 03:03:48 +0000

    g

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,6 @@
 # This Puppet manifest kills a process named "killmenow" using pkill
-
 exec { 'killmenow':
   command     => 'pkill -f killmenow',
-  path        => ['/usr/bin', '/usr/sbin'],
-  refreshonly => true,
+  path        => '/bin:/usr/bin',
 }
 
