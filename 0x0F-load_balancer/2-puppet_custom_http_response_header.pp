fd9350a9c7412a64c82a66c91eefd4a3e2338758
Obi <williamobi818@gmail.com>
2023-10-02 19:11:17 +0000

    trial 03

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,7 +2,7 @@
 
 # Ensure the package manager's cache is up to date
 exec { 'apt_update':
-  command => '/usr/bin/apt-get update'
+  command => '/usr/bin/apt-get update',
 }
 
 # Install Nginx
@@ -19,35 +19,25 @@ service { 'nginx':
 }
 
 # Configure Nginx
-class { 'nginx': }
-
-nginx::resource::server { '_':
-  listen_port => 80,
-  index_files => ['index.html'],
-  use_default_location => false,
-  www_root => '/var/www/html',
+file { '/etc/nginx/sites-available/default':
+  content => "\
+server {
+    listen 80;
+    server_name _;
+    root /var/www/html;
+    index index.html;
+    location / {
+        add_header Content-Type 'text/html';
+        add_header Accept-Ranges 'bytes';
+        add_header X-Served-By $hostname;
+        return 200 'Hello World!\\n';
+    }
+    location /redirect_me {
+        return 301 'https://www.youtube.com/watch?v=QH2-TGUlwu4';
+    }
 }
-
-nginx::resource::location { '/':
-  location => '/',
-  server => '_',
-  location_custom_cfg => {
-    'add_header' => [
-      'Content-Type "text/html"',
-      'ETag "your-etag-value"',
-      'Accept-Ranges "bytes"',
-      'X-Served-By \$hostname',
-    ],
-    'return' => '200 "Hello World!\n"',
-  },
-}
-
-nginx::resource::location { '/redirect_me':
-  location => '/redirect_me',
-  server => '_',
-  location_custom_cfg => {
-    'return' => '301 https://www.youtube.com/watch?v=QH2-TGUlwu4',
-  },
+",
+  require => Package['nginx'],
 }
 
 # Restart Nginx service after configuration
