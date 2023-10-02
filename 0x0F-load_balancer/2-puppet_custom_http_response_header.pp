a74c979afe92ebff6f1c0f50febb68c7b311d8a2
Obi <williamobi818@gmail.com>
2023-10-02 18:51:31 +0000

    trial 01

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,36 +1,33 @@
 # 2-puppet_custom_http_response_header
 
-# Ensure Nginx is installed
-package { 'nginx':
-  ensure => 'installed',
+class { 'nginx': }
+
+nginx::resource::server { '_':
+  listen_port => 80,
+  index_files => ['index.html'],
+  use_default_location => false,
+  www_root => '/var/www/html',
 }
 
-# Define Nginx configuration
-file { '/etc/nginx/sites-available/default':
-  content => "\
-server {
-    listen 80;
-    server_name _;
-    root /var/www/html;
-    index index.html;
-    location / {
-        add_header Content-Type 'text/html';
-        add_header Accept-Ranges 'bytes';
-        add_header X-Served-By $hostname;
-        return 200 'Hello World!\\n';
-    }
-    location /redirect_me {
-        return 301 'https://www.youtube.com/watch?v=QH2-TGUlwu4';
-    }
-}
-",
-  require => Package['nginx'],
+nginx::resource::location { '/':
+  location => '/',
+  server => '_',
+  location_custom_cfg => {
+    'add_header' => [
+      'Content-Type "text/html"',
+      'ETag "your-etag-value"',
+      'Accept-Ranges "bytes"',
+      'X-Served-By $hostname',
+    ],
+    'return' => '200 "Hello World!\n"',
+  },
 }
 
-# Restart Nginx
-service { 'nginx':
-  ensure    => 'running',
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-available/default'],
+nginx::resource::location { '/redirect_me':
+  location => '/redirect_me',
+  server => '_',
+  location_custom_cfg => {
+    'return' => '301 https://www.youtube.com/watch?v=QH2-TGUlwu4',
+  },
 }
 
