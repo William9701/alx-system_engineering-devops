e84ad01726d7ed1572e5ffb6fa699c93ee2df127
Obi <williamobi818@gmail.com>
2023-11-16 22:00:38 +0000

    web_stack_debugging

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,14 @@
+# testing how well our web server setup featuring Nginx is doing under pressure
+
+# top up hard file limit for user
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# top up soft file limit for user holberton
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
---
+++ 0x1B-web_stack_debugging_4/README.md
@@ -0,0 +1,1 @@
+this repo is for web_stack_debugging_4
